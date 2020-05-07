8a7033518f2595140590b7b56399b0cc7ce72d9d
MerrickZ <anphorea@gmail.com>
2020-05-07 17:35:46 +0800

    change to system default encoding

--- 8.CRC32-in-current-directory/CRC32.py
+++ 8.CRC32-in-current-directory/CRC32.py
@@ -1,18 +1,24 @@
-#/bin/env python
+# /bin/env python
 
+import os
+import sys
 import zlib
-import os,sys
+import locale
+
 
 def crc(fileName):
-	prev = 0
-	for eachLine in open(fileName,"rb"):
-		prev = zlib.crc32(eachLine, prev)
-	return "%8X"%(prev & 0xFFFFFFFF)
+    prev = 0
+    for eachLine in open(fileName, "rb"):
+        prev = zlib.crc32(eachLine, prev)
+    return "%8X" % (prev & 0xFFFFFFFF)
 
-with open("crc32.txt","w+",encoding="GBK") as output:
-	op=["[-CRC-32-]FILE\n","--------------\n"]
-	for i in os.listdir():
-		if os.path.isfile(i) and not "crc" in i.lower() :
-			print(f"[{crc(i)}]{i}")
-			op.append(f"[{crc(i)}]{i}\n")
-	output.writelines(op)
+
+current_encoding = locale.getpreferredencoding()
+
+with open("crc32.txt", "w+", encoding=current_encoding) as output:
+    op = ["[-CRC-32-] FileName\n", "--------------\n"]
+    for i in os.listdir():
+        if os.path.isfile(i) and not "crc" in i.lower():
+            print(f"[{crc(i)}]{i}")
+            op.append(f"[{crc(i)}]{i}\n")
+    output.writelines(op)
