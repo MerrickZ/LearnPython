a3832d1d9bffb99648957190fde44eaf74f2f145
MerrickZ <anphorea@gmail.com>
2020-05-07 15:45:17 +0800

    fix logic error

--- 5.trimPDF/trimpdf.py
+++ 5.trimPDF/trimpdf.py
@@ -5,37 +5,43 @@ import sys
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
+#########################################
+# Author: MerrickZ (anphorea@gmail.com) #
+#########################################
+
+# This script is used to remove some ad pages from PDFs by verifing the page height and width.
 
 def deletepagesFromPdf(pdfin, pdfout):
-    pdf_out = PdfFileWriter()
-    with open(pdfin, 'rb') as f:
-        pdf_src = PdfFileReader(f)
-        ipages = pdf_src.getNumPages()
-        print(f"Original PDF Pages: {ipages}")
-        for i in range(0,ipages):
-            page_src = pdf_src.getPage(i)
-            if page_src.mediaBox[2] == 960 and page_src.mediaBox[3] == 540:
-                continue
-            if page_src.mediaBox[2] == 294.24 and page_src.mediaBox[3] == 206.4:
-                continue
-            pdf_out.addPage(page_src)
-        print(f"New PDF Pages: {pdf_out.getNumPages()}")
-        if pdf_out.getNumPages() + 2 < ipages:
-            # all same size, leave it be.
-            return
-    with open(pdfout,'wb') as o:
-        pdf_out.write(o)
+    print(f"{pdfin} --> {pdfout}")
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(pdfin)
+    ipages = pdf_reader.getNumPages()
+    print(f"Original PDF Pages: {ipages}")
+    for i in range(ipages):
+        page_src = pdf_reader.getPage(i)
+        if (page_src.mediaBox[2] == 960) and (page_src.mediaBox[3] == 540):
+            continue
+        if (page_src.mediaBox[2] == 294.24) and (page_src.mediaBox[3] == 206.4):
+            continue
+        pdf_writer.addPage(page_src)
+    print(f"New PDF Pages: {pdf_writer.getNumPages()}")
+    if pdf_writer.getNumPages() + 2 < ipages:
+        # all same size, leave it be.
+        return
+    with open(pdfout, 'wb') as o:
+        pdf_writer.write(o)
     os.remove(pdfin)
-            
 
-def sort_into_directories(cdir,filename):
-    years=["2020","2019","2018",'2017','2016','2015']
+
+def sort_into_directories(cdir, filename):
+    years = ["2020", "2019", "2018", '2017', '2016', '2015']
     for i in years:
         if i in filename:
-            if ( not os.path.exists(i)) :
+            if (not os.path.exists(i)):
                 os.mkdir(i)
-            deletepagesFromPdf(filename,os.path.join(cdir,i,filename))
+            deletepagesFromPdf(filename, os.path.join(cdir, i, filename))
             return
+
 
 if __name__ == '__main__':
     print('清理当前目录下的PDF文件')
@@ -47,6 +53,6 @@ if __name__ == '__main__':
         print("处理文件： %s" % f)
         os.chmod(f, stat.S_IWRITE)
         try:
-            sort_into_directories(current_dir,f)
+            sort_into_directories(current_dir, f)
         except Exception as e:
             print("处理失败 %s" % e)
